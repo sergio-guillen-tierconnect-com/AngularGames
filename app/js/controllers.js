df1e5f2e4440c44f46894a9dfc7cf0b404e0f1d4
Sergio Guillen <serguimant@hotmail.com>
2013-05-14 12:08:52 -0400

    added row highlighting on players table

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -2,23 +2,30 @@
 
 /* Controllers */
 
-angular.module('myApp.controllers', []).
-  controller('MyCtrl1', [function() {
-
-  }])
-  .controller('MyCtrl2', [function() {
-
-  }]);
+angular.module('myApp.controllers', []);
 
 function BodyCtrl ($scope) {
   // Para header-bar
   $scope.accountBalance = 30;
-  // Para points bank
-  $scope.pointsBank = 20;
 }
 
-function DraftTeamCtrl ($scope, $http) {
+/* Uses $rootScope in order to have global variables
+ * in this case pointsBank is a global variable, it is defined
+ * in run method (app.js)
+ */
+function DraftTeamCtrl ($scope, $http, $rootScope) {
   $http.get('data/players.json').success(function(data) {
     $scope.challenge = data;
+    if ($scope.challenge.FantasyChallenge.PositionOneAvailablePlayerList.length > 0) {
+      $rootScope.pointsBank = $scope.challenge.FantasyChallenge.PositionOneAvailablePlayerList[0].FantasyCost;
+    }
   });
+  
+  $scope.selectPlayer = function(player) {
+    $rootScope.pointsBank = player.FantasyCost;
+    
+    // Used to change class for selected table row
+    // class is called "active" in app.css
+    $scope.active = player;
+  }
 }
